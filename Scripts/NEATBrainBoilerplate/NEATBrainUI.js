79d3cabb438a1c733a668bbfbf2ae6b866df624c
Cody <nytemairqt@gmail.com>
2024-01-03 08:57:22 +1000

    Moved UI constructor methods to separate file

--- Scripts/NEATBrainBoilerplate/NEATBrainUI.js
+++ Scripts/NEATBrainBoilerplate/NEATBrainUI.js
@@ -1,5 +1,6 @@
 include("NEATBRAINBoilerplate/NEATBrainLookAndFeel.js");
 include("NEATBRAINBoilerplate/NEATBrainModules.js");
+include("NEATBRAINBoilerplate/NEATBrainUIConstructors.js");
 
 const SliderPack_RatiosL = Content.getComponent("SliderPack_RatiosL");
 const SliderPack_RatiosR = Content.getComponent("SliderPack_RatiosR");
@@ -9,75 +10,8 @@ const SliderPack_RatiosR = Content.getComponent("SliderPack_RatiosR");
 for (i = 0; i < 128; i++)
    	Engine.setKeyColour(i, Colours.withAlpha(Colours.black, 0.2));
    	
-/* Constructors */
-   	
-inline function createButton(id, x, y, width, height, text, saveInPreset, callback, isMomentary, visible)
-{
-	local b = Content.addButton(id, x, y);
-	
-	b.set("width", width);
-	b.set("height", height);
-	b.set("text", text);
-	b.set("saveInPreset", saveInPreset);
-	b.set("isMomentary", isMomentary);
-	b.set("visible", visible);
-	b.setControlCallback(callback);
-	
-	Content.setPropertiesFromJSON(id, {
-	          parentComponent: "pnlBody"
-		});
-	
-	return b;
-}
 
-inline function createKnob(id, x, y, width, height, text, saveInPreset, callback, minValue, maxValue, stepSize, defaultValue)
-{
-	local k = Content.addKnob(id, x, y);
-	
-	k.set("text", text);
-	k.set("min", minValue);
-	k.set("max", maxValue);
-	k.set("stepSize", stepSize);
-	k.set("defaultValue", defaultValue);
-	k.set("width", width);
-	k.set("height", height);
-	
-	k.set("saveInPreset", saveInPreset);
-	k.setControlCallback(callback);
 
-	Content.setPropertiesFromJSON(id, {
-          parentComponent: "pnlBody"
-	});
-
-	k.setLocalLookAndFeel(LAFSliderNEATBrain); // move this to outside the constructor...
-	
-	return k;
-}
-
-inline function createTextPanel(id, x, y, width, height, fontSize, text)
-{
-	local p = Content.addPanel(id, x, y);
-
-	p.set("width", width);
-	p.set("height", height);
-	p.set("text", text);
-
-	p.data.text = text;
-	p.data.fontSize = fontSize;
-	
-	Content.setPropertiesFromJSON(id, {
-          parentComponent: "pnlBody"
-	});
-
-	p.setPaintRoutine(function(g)
-	{
-		g.fillAll(Colours.withAlpha(Colours.grey, 0.0));
-		g.setColour(Colours.withAlpha(Colours.grey, 1.0));
-		g.setFont("bold", this.data.fontSize);
-		g.drawAlignedText(this.data.text, [0, 0, this.getWidth(), this.getHeight()], "centred");
-
-	});
-}
 
 /* UI Control Methods */
 
@@ -168,23 +102,23 @@ const btnShowAdvancedPanel = createButton("btnShowAdvancedPanel", 471, 200, 24, 
 
 /* Instantiate Sliders */
 
-const knbFilter = createKnob("knbFilter", 100, 400, 64, 64, "Filter", true, onknbFilterControl, 300, 4000, 1.0, 1200);
-const knbDampening = createKnob("knbDampening", 500, 400, 64, 64, "Dampening", true, onknbDampeningControl, 0.0, 1.0, 0.01, 0.0);
+const knbFilter = createKnob("knbFilter", 100, 400, 48, 48, "Filter", true, onknbFilterControl, 300, 4000, 1.0, 1200);
+const knbDampening = createKnob("knbDampening", 500, 400, 48, 48, "Dampening", true, onknbDampeningControl, 0.0, 1.0, 0.01, 0.0);
 
-const knbAttack = createKnob("knbAttack", 565, 120, 64, 64, "Attack", true, onknbAttackControl, 5, 1000, 1.0, 5);
-const knbDecay = createKnob("knbDecay", 665, 120, 64, 64, "Decay", true, onknbDecayControl, 500, 20000, 1.0, 15000);
-const knbSustain = createKnob("knbSustain", 765, 120, 64, 64, "Sustain", true, onknbSustainControl, -100, 0, 1.0, -100);
-const knbRelease = createKnob("knbRelease", 865, 120, 64, 64, "Release", true, onknbReleaseControl, 5, 15000, 1.0, 200);
+const knbAttack = createKnob("knbAttack", 565, 120, 48, 48, "Attack", true, onknbAttackControl, 5, 1000, 1.0, 5);
+const knbDecay = createKnob("knbDecay", 665, 120, 48, 48, "Decay", true, onknbDecayControl, 500, 20000, 1.0, 15000);
+const knbSustain = createKnob("knbSustain", 765, 120, 48, 48, "Sustain", true, onknbSustainControl, -100, 0, 1.0, -100);
+const knbRelease = createKnob("knbRelease", 865, 120, 48, 48, "Release", true, onknbReleaseControl, 5, 15000, 1.0, 200);
 
 /* Instantiate Labels */
 
-const lblResidue = createTextPanel("lblResidue", 20, 10, 128, 64, 24, "Residue_");
-const lblModes = createTextPanel("lblModes", 480, 10, 128, 64, 24, "Modes_");
+const lblResidue = createLabel("lblResidue", 487, 10, 128, 64, 24, "Residue_");
+const lblModes = createLabel("lblModes", 15, 10, 128, 64, 24, "Modes_");
 
-const lblAttack = createTextPanel("lblAttack", 535, 180, 128, 32, 16, "Attack");
-const lblDecay = createTextPanel("lblDecay", 632, 180, 128, 32, 16, "Decay");
-const lblSustain = createTextPanel("lblSustain", 733, 180, 128, 32, 16, "Sustain");
-const lblRelease = createTextPanel("lblRelease", 836, 180, 128, 32, 16, "Release");
+const lblAttack = createLabel("lblAttack", 535, 180, 128, 32, 16, "Attack");
+const lblDecay = createLabel("lblDecay", 632, 180, 128, 32, 16, "Decay");
+const lblSustain = createLabel("lblSustain", 733, 180, 128, 32, 16, "Sustain");
+const lblRelease = createLabel("lblRelease", 836, 180, 128, 32, 16, "Release");
 
 /* Setup Misc Defaults */
 
---
+++ Scripts/NEATBrainBoilerplate/NEATBrainUIConstructors.js
@@ -0,0 +1,94 @@
+/* Constructors */
+   	
+inline function createButton(id, x, y, width, height, text, saveInPreset, callback, isMomentary, visible)
+{
+	local b = Content.addButton(id, x, y);
+	
+	b.set("width", width);
+	b.set("height", height);
+	b.set("text", text);
+	b.set("saveInPreset", saveInPreset);
+	b.set("isMomentary", isMomentary);
+	b.set("visible", visible);
+	b.setControlCallback(callback);
+	
+	Content.setPropertiesFromJSON(id, {
+	          parentComponent: "pnlBody"
+		});
+	
+	return b;
+}
+
+inline function createKnob(id, x, y, width, height, text, saveInPreset, callback, minValue, maxValue, stepSize, defaultValue)
+{
+	local k = Content.addKnob(id, x, y);
+	
+	k.set("text", text);
+	k.set("min", minValue);
+	k.set("max", maxValue);
+	k.set("stepSize", stepSize);
+	k.set("defaultValue", defaultValue);
+	k.set("width", width);
+	k.set("height", height);
+	
+	k.set("saveInPreset", saveInPreset);
+	k.setControlCallback(callback);
+
+	Content.setPropertiesFromJSON(id, {
+          parentComponent: "pnlBody"
+	});
+
+	k.setLocalLookAndFeel(LAFSliderNEATBrain); // move this to outside the constructor...
+	
+	return k;
+}
+
+inline function createLabel(id, x, y, width, height, fontSize, text)
+{
+	local p = Content.addPanel(id, x, y);
+
+	p.set("width", width);
+	p.set("height", height);
+	p.set("text", text);
+
+	p.data.text = text;
+	p.data.fontSize = fontSize;
+	
+	Content.setPropertiesFromJSON(id, {
+          parentComponent: "pnlBody"
+	});
+
+	p.setPaintRoutine(function(g)
+	{
+		g.fillAll(Colours.withAlpha(Colours.grey, 0.0));
+		g.setColour(Colours.withAlpha(Colours.grey, 1.0));
+		g.setFont("bold", this.data.fontSize);
+		g.drawAlignedText(this.data.text, [0, 0, this.getWidth(), this.getHeight()], "centred");
+
+	});
+	
+	return p;
+}
+
+inline function createChildPanel(id, x, y, width, height)
+{
+	local p = Content.addPanel(id, x, y);
+
+	p.set("width", width);
+	p.set("height", height);
+	
+	Content.setPropertiesFromJSON(id, {
+          parentComponent: "pnlBody"
+	});
+
+	p.setPaintRoutine(function(g)
+	{
+		g.fillAll(Colours.withAlpha(Colours.grey, 0.0));
+		g.setColour(Colours.withAlpha(Colours.grey, 1.0));
+		g.setFont("bold", this.data.fontSize);
+		g.drawAlignedText(this.data.text, [0, 0, this.getWidth(), this.getHeight()], "centred");
+
+	});
+	
+	return p;
+}
