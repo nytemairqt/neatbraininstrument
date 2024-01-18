f43553f40e0e7ee12e086cc498b505fb24ee7c80
Cody <nytemairqt@gmail.com>
2024-01-18 13:34:16 +1000

    Improved sound model

--- Scripts/ScriptProcessors/neatbraininstrument/Interface.js
+++ Scripts/ScriptProcessors/neatbraininstrument/Interface.js
@@ -1,8 +1,5 @@
 Content.makeFrontInterface(1000, 710);
 
-
-Engine.loadAudioFilesIntoPool();
-
 include("RhapsodyBoilerplate/includes/Ui.js");
 include("RhapsodyBoilerplate/includes/LookAndFeel.js");
 include("RhapsodyBoilerplate/includes/Paths.js");
@@ -13,10 +10,29 @@ include("RhapsodyBoilerplate/includes/Presets.js");
 include("RhapsodyBoilerplate/includes/UserSettings.js");
 include("RhapsodyBoilerplate/includes/Spinner.js");
 
-
 /* NEATBrain External Files */
 
 include("NEATBRAINBoilerplate/NEATBrainUi.js");
+
+/* Managing WaveTables */
+
+const MEMORYNAME = "Achromic";
+
+Engine.loadAudioFilesIntoPool();
+
+const audioFiles = FileSystem.getFolder(FileSystem.AudioFiles);
+const subfolder = audioFiles.createDirectory(MEMORYNAME);
+
+reg rr = 0;
+
+/*
+	on load, check for wavetables in "subfolder"
+	if they don't exist, prompt user to "build wavetables" with a popup panel
+	when user builds wavetables, extract them from a zip in the samples zip
+	
+	NEED TO SHIP .hwt files along with either the Samples or the .HXI
+
+*/
 function onNoteOn()
 {
 	
