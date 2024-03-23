f5516af9798fad2043eb11a682c8245de4b3e32a
Cody <nytemairqt@gmail.com>
2024-03-23 15:21:13 +1000

    improved Browse Instruments button LAF

--- Scripts/NEATBrainBoilerplate/NEATBrainPatreon.js
+++ Scripts/NEATBrainBoilerplate/NEATBrainPatreon.js
@@ -42,15 +42,13 @@ pnlStore.setPaintRoutine(function(g)
 {
 	g.setColour(Colours.withAlpha(0xFFCFCFCF, this.data.hover ? 1.0 : 0.8));
 	
+
 	// Body
-	g.drawLine(2, 16, 6, 6, 2.0); // Top
-	g.drawLine(3, 15, 16, 16, 2.0); // Bottom
-	g.drawLine(2, 3, 6, 16, 2.0); // Angle Left
-	g.drawLine(16, 15, 6, 16, 2.0); // Angle Right
+	g.fillRect([2, 5, 14, 14]);
 	
 	// Handle
-	g.drawLine(5, 5, 5, 2, 2.0); 
-	g.drawLine(13, 13, 5, 2, 2.0);
-	g.drawLine(5, 13, 2, 2, 2.0);
+	g.drawLine(5, 5, 5, 1, 2.0); 
+	g.drawLine(13, 13, 5, 1, 2.0);
+	g.drawLine(6, 12, 2, 2, 2.0);
 	
 });
