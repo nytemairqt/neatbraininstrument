31d8476a4113e61d45ec024e390588aae402e817
Cody <nytemairqt@gmail.com>
2024-03-25 10:39:44 +1000

    Proliferated GPL License

--- Scripts/NEATBrainBoilerplate/NEATBrainPathData.js
+++ Scripts/NEATBrainBoilerplate/NEATBrainPathData.js
@@ -1,3 +1,20 @@
+/*
+    Copyright 2023, 2024 iamlamprey
+
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with This file. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const pathResetButtonStroke = [110,109,0,0,32,64,66,96,101,64,98,104,145,77,64,217,206,39,64,229,208,138,64,98,16,0,64,248,83,179,64,98,16,0,64,98,197,32,248,64,98,16,0,64,0,0,24,65,131,192,114,64,0,0,24,65,0,0,192,64,98,0,0,24,65,223,79,3,65,197,32,248,64,231,251,31,65,248,83,179,
 64,231,251,31,65,98,106,188,148,64,231,251,31,65,213,120,113,64,236,81,26,65,25,4,70,64,145,237,16,65,101,0,0 ];
 
